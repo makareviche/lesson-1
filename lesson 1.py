d2dd03bc2f715c73d24fd2d587265b1d1ecabb96
makareviche <makarevichee@gmail.com>
2024-10-21 20:50:44 +0300

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,9 @@
+# 'module_1_3.py'
+name = 'evgeni'
+age = 32
+'its_student'
+true = 'its_student'
+print('name:',name)
+print('age:',age)
+print('age_nu:',age + 1)
+print('true its_student:',(true == 'its_student'))
